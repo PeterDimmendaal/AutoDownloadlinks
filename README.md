69fdc3a4264991c727a46af609d1197d88765d46
joep-hanze <55184540+joep-hanze@users.noreply.github.com>
2019-12-31 13:05:06 +0100

    Includes track id's of 'next' url. Added config for general settings. Saves tracks in correct format.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AutoDownloadlinks
 For dz downloader: Automaticly add track links from playlist to downloadlinks.txt
-Make sure you have Python installed.
+Make sure you have Python 3 installed.
 
-1. Edit GetPlaylist.py with notepad and enter your downloadlinks.txt
+1. Edit CONFIG.ini with notepad and enter your downloadlinks.txt location and deezer playlist id
 2. Run GetPlaylist.py
--- getPlaylist.py
+++ getPlaylist.py
@@ -1,27 +1,44 @@
 import requests
-import json
+from configparser import ConfigParser
 
-#change this to the file location of your downloadlinks.txt
-path = "C:\\Users\\Peter\\Music download"
-playlistid = input("Paste the playlistID")
+def get_track_ids_from_url(url):
+    # Get JSON data from url    
+    resp = requests.get(url=url) 
+    tracks_json = resp.json()    
+    # Save all track ID's from JSON data
+    track_ids = []
+    try: 
+        tracks = tracks_json["data"]
+        for track in tracks:
+            track_ids.append(track['id'])
+    except:
+        print("Something went wrong, check if the playlist id is correct and you have a working connection.")        
+    # Check if url contains more tracks
+    try: 
+        url_next = tracks_json["next"]
+        next_tracks = get_track_ids_from_url(url_next)
+        track_ids.extend(next_tracks)
+    except:
+        pass # 'next' is not always present    
+    return track_ids
 
-url = 'http://api.deezer.com/playlist/'+playlistid +'/tracks'
+def save_ids_to_file(ids, path):
+        try:            
+            downloadlinksFile = open(str(path)+"/downloadLinks.txt","w" )
+            for id_ in ids:
+                downloadlinksFile.write("https://www.deezer.com/track/"+str(id_)+"\n")
+            print("Succesfully downloaded "+str(len(ids))+" tracks.")
+        except:
+            print("Something went wrong, check if ... is correct.")
 
+# Read data from config
+config = ConfigParser()
+config.read('CONFIG.ini')
+path = config['main']['path_downloadlinks']
+playlist_id = config['main']['playlistid']
 
-
-resp = requests.get(url=url) 
-tracksJson = resp.json()
-try: 
-    Tracks = tracksJson["data"]
-    downloadlinksFile = open(path+ "/downloadLinks.txt","w" )
-    for track in Tracks:
-        downloadlinksFile.write(track['link']+ "\n")
-    print("succes")
-except:
-    print("Something went wrong, check if the playlist id is correct and you have a working connection")
-
-input()
-
-    
-
+url = 'http://api.deezer.com/playlist/'+str(playlist_id)+'/tracks'
+track_ids = get_track_ids_from_url(url)
+if (track_ids):
+    save_ids_to_file(track_ids, path)
 
